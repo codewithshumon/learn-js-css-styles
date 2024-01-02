a2c14db5ef12aea5b38548d146621698596abff3
Shumon Khan <backlink.shumonkhan@gmail.com>
2024-01-02 17:37:37 +0600

    Mouse mask reavel background text with page transition

---
+++ mouse-mask-page-transition/script.js
@@ -0,0 +1,21 @@
+// Tutorial video link: https://www.youtube.com/watch?v=y6z8MYjZ_J8
+
+const span = document.querySelector('.hover-btn2');
+const overlay = document.querySelector('.overlay');
+
+window.addEventListener('mousemove', (e) => {
+  const { clientX, clientY } = e;
+  const x = Math.round((clientX / window.innerWidth) * 100);
+  const y = Math.round((clientY / window.innerHeight) * 100);
+
+  gsap.to(overlay, {
+    '--x': `${x}%`,
+    '--y': `${y}%`,
+    duration: 0.3,
+    ease: 'sine.out',
+  });
+});
+
+span.addEventListener('click', () => {
+  overlay.classList.toggle('is-open');
+});
