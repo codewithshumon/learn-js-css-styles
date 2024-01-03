f682ac0fbd74ec8b4df47a5f1b78c7adcba56f0e
Shumon Khan <backlink.shumonkhan@gmail.com>
2024-01-03 17:46:33 +0600

    background noice animation or Grainy texture animation

---
+++ background-noice-animation/script.js
@@ -0,0 +1,10 @@
+// Grainy texture animation by Geoff Graham https://css-tricks.com/snippets/css/animated-grainy-texture/
+
+// const sountDiv = document.querySelector('.footer-sound');
+// const sbar = document.querySelectorAll('.sbar');
+
+// sountDiv.addEventListener('click', () => {
+//   sbar.forEach((element) => {
+//     element.classList.toggle('noAnim');
+//   });
+// });
